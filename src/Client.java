c3fdec3e2a502eaf6921d1ccdf004dee219f22ec
smgA4C9 <ghjkgjkghjk>
2024-10-10 18:55:26 +0300

    initial commit

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client {
+    private String name;
+    private List<Book> boughtBooks;
+    private List<Book> wishList;
+
+    public Client(String name, List<Book> boughtBooks, List<Book> wishList) {
+        this.name = name;
+        this.boughtBooks = boughtBooks;
+        this.wishList = wishList;
+    }
+
+    public Client() {
+        name="Unknown client";
+        boughtBooks=new ArrayList<>();
+        wishList=new ArrayList<>();
+    }
+
+    public List<Book> getBoughtBooks() {
+        return boughtBooks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getWishList() {
+        return wishList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBoughtBooks(List<Book> boughtBooks) {
+        this.boughtBooks = boughtBooks;
+    }
+
+    public void setWishList(List<Book> wishList) {
+        this.wishList = wishList;
+    }
 }
